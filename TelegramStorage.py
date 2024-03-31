4f70f68c64d87fedb39420a97b4f839088e040f2
Andrey Izman <izmanw@gmail.com>
2024-03-31 06:11:48 +0300

    Clean up

--- TelegramStorage.py
+++ TelegramStorage.py
@@ -83,66 +83,44 @@ def parse_album_info(content):
         genre = ' / '.join(genres)
     return artist, title, year, genre, genres
 
-TOK_START_REACHED = 1
-TOK_END_REACHED = 2
 
 class TgLoader:
+    def __str__(self) -> str:
+        return f'TgLoader <{self.chat_id}>'
+
     def __init__(self, chat_id, add_entry):
         self.api = TelegramStorage.loaded().api
-        # init playlist from DB
         self.playlist = TgPlaylist.read(chat_id)
         self.chat_id = chat_id
         self.add_entry = add_entry
-        self._terminated = False
-        self._index = 1
         self.segment = [0,0]
         self.offset_msg_id = 0
-        self.token = None
         self.page = 0
-
-#     def save():
-#         # save cursors to storage
-#         if self._index == 0 and self.playlist.segments[0][0] == 0:
-#             pass
-#         self.playlist.save()
-#         self.playlist.reload()
+        self._end_of_page = False
+        self._terminated = False
+        self._loaded = False
 
     def start(self):
-        print('===============================================')
         print(f'TgLoader.start')
         print(self.playlist.segments)
         self.playlist.segments.insert(0, [0,0])
         self.segment = self.playlist.segment(1)
-        # start loading
         blob = {}
-#         if len(self.playlist.segments):
-#             blob["last_msg_id"] = self.playlist.segments[0][0]
         self.load(blob, limit=1)
         return self
 
     def update(self, audio):
         # update audio, add entries to playlist
-#         self.add_entry(TgAudio(data))
         if audio.message_id == self.segment[0]:
-            self.token = TOK_START_REACHED
+            self._end_of_page = True
         if audio.message_id == self.segment[1]:
-            self.token = TOK_END_REACHED
-        if not self.token:
+            self._end_of_page = True
+        if not self._end_of_page:
             self.add_entry(audio)
 
     def next(self, blob, cmd):
         self.page = self.page + 1
-#         done = cmd == 'DONE'
-#         if cmd == 'DONE':
-#             done = True
-#             blob = {"last_msg_id": -1}
-
-        print('===============================================')
         print('TgLoader.next %s' % cmd)
-#         print(blob)
-
-#         first_message_id = blob.get('first_message_id', 0)
-#         offset_msg_id = blob.get('offset_msg_id', 0)
         last_msg_id = blob.get('last_msg_id', 0)
 
         if self.playlist.segments[0][0] == 0:
@@ -150,17 +128,17 @@ class TgLoader:
         self.playlist.segments[0][1] = last_msg_id
         offset_msg_id = last_msg_id
 
-        if self.token:
+        if self._end_of_page:
             self.playlist.segments[0][1] = self.segment[1]
             offset_msg_id = self.segment[1]
             del self.playlist.segments[1]
 
-        self.token = None
+        self._end_of_page = False
 
         if cmd == 'DONE' or offset_msg_id == -1:
             self.playlist.segments = [[ self.playlist.segments[0][0], -1]]
             self.playlist.save()
-            self.stop()
+            self._loaded = True
             print('tg.loader.DONE')
             return
 
@@ -173,75 +151,24 @@ class TgLoader:
             return
 
         self.offset_msg_id = offset_msg_id
-#         self.load({"offset_msg_id": offset_msg_id})
         print('timeout_add')
         GLib.timeout_add(5000 if self.page > 10 else 1000, self.load, {"offset_msg_id": offset_msg_id})
 
-#         if cmd != 'DONE':
-#             self.playlist.segments.insert(self._index, [
-#                 offset_msg_id if offset_msg_id != 0 else first_message_id, last_msg_id
-#             ])
-#         else:
-#             if len(self.playlist.segments) < self._index + 1:
-#                 self.playlist.segments.append([0,0])
-#             self.playlist.segments[self._index][0] = offset_msg_id if offset_msg_id != 0 else first_message_id
-#             self.playlist.segments[self._index][1] = -1
-#             self.playlist.save()
-# #             self.playlist.reload()
-#
-#         self._index = self._index + 1
-#
-#         if cmd == 'END_OF_SEGMENT':
-#             self._index = self._index + 1
-
-#         if cmd != 'DONE' and offset_msg_id != -1:
-#             self.load({"offset_msg_id": offset_msg_id})
-#         else:
-#             self.stop()
-#             if len(self.playlist.segments) >= self._index + 1:
-#                 blob["last_msg_id"] = self.playlist.segments[self._index - 1][0]
-#
-#             if blob["offset_msg_id"] != -1:
-
-        # save cursors to storage
-#         if self._index == 0 and self.playlist.segments[0][0] == 0:
-#         self.playlist.segments[self._index][0] = offset_msg_id if offset_msg_id != 0 else first_message_id
-#         self.playlist.segments[self._index][1] = last_msg_id
-#         self.playlist.save()
-#         self.playlist.reload()
-
-#         if not done:
-#           self._index = self._index + 1
-#           self.playlist.segment.insert(self._index, [0,0])
-
-#         if not blob or blob is None:
-#             # no result
-#             pass
-#         # next iteration
-#         pass
-
     def stop(self):
-        # stop loading process
-        print('===============================================')
         print(f'TgLoader.stop')
         self._terminated = True
-        # save vars to DB
-        # stop loading
-        pass
 
     def load(self, blob={}, limit=50):
-        # loading data
         if not self._terminated:
-            print('===============================================')
             print(f'TgLoader.load  {self.chat_id}')
             print(blob)
             self.api.load_messages_idle( self.chat_id, update=self.update, done=self.next, blob={**blob}, limit=limit)
 
-    def __str__(self) -> str:
-        return f'TgLoader <{self.chat_id}>'
-
 
 class TgPlaylist:
+    def __str__(self) -> str:
+        return f'TgPlaylist <{self.chat_id}>'
+
     def __init__(self, data):
         self._data = data
         self.update(data)
@@ -259,12 +186,6 @@ class TgPlaylist:
             return self.segments[index]
         return [0, 0]
 
-#     def set_segment(self, index):
-#         if len(self.segments) > index:
-#             return self.segments[index]
-#         self.segments.insert(0, [0, 0])
-#         return self.segments[index]
-
     @staticmethod
     def read(chat_id):
         playlist = TelegramStorage.loaded().select('playlist', {"chat_id": chat_id})
@@ -275,51 +196,23 @@ class TgPlaylist:
         self._data = TgPlaylist.read(self.chat_id)._data
 
     def save(self):
-        print('===============================================')
         print('TgPlaylist.save')
-#         segments = [[0,0]]
-#         for segment in self.segments:
-#             if segments[-1][0] == 0:
-#                 segments[-1] = segment
-#             elif segment[-1][1] == segments[0]:
-#                 segments[-1][1] = segment[1]
-#             else:
-#                 segment.append(segment)
-#
-# def seg(_segments):
-#     segments = [[0,0]]
-#     for segment in _segments:
-#         if segments[-1][0] == 0:
-#             segments[-1] = segment
-#         elif segment[-1][1] == segments[0]:
-#             segments[-1][1] = segment[1]
-#         else:
-#             segment.append(segment)
-#     return segments
-#
-#             print(self.segments)
-#             print(segments)
-
-
         print(self.segments)
-#         print(segments)
-#         self.segments = segments
         playlist = {
             "chat_id": self.chat_id,
             "title": self.title,
             "original_title": self.original_title,
             "segments": json.dumps(self.segments)
-#             "segments": json.dumps(segments)
         }
         if self.id != 0:
             return TelegramStorage.loaded().update('playlist', playlist, {"chat_id": self.chat_id}, 1)
         return TelegramStorage.loaded().insert('playlist', playlist)
 
-    def __str__(self) -> str:
-        return f'TgPlaylist <{self.id}>'
-
 
 class TgAudio:
+    def __str__(self) -> str:
+        return f'TgAudio <{self.message_id}>'
+
     def __init__(self, data):
         self._data = data
         self.update(data)
@@ -378,8 +271,6 @@ class TgAudio:
                 return self.local_path
             else:
                 def on_done(data):
-                    print('=========done========')
-                    print(data)
                     # update = TelegramApi.loaded().add_audio(data, convert=False)
                     self.update(data)
                     done(self)
@@ -389,12 +280,12 @@ class TgAudio:
 
         return self.local_path
 
-    def __str__(self) -> str:
-        return f'TgAudio <{self._data}>'
-
 
 class TelegramStorage:
     __instance = None
+
+    def __str__(self) -> str:
+        return f'TelegramStorage <{self.api.hash}>'
 
     def __init__(self, api, files_dir):
         self.api = api
@@ -415,7 +306,7 @@ class TelegramStorage:
                 self.db.execute(SQL.TABLE_PLAYLIST)
                 self.db.execute(SQL.TABLE_AUDIO)
                 # self.db.execute(SQL.TABLE_DOCUMENT)
-                self.db.execute(SQL.TABLE_INFO)
+#                 self.db.execute(SQL.TABLE_INFO)
             except Exception as e:
                 os.remove(self.db_file)
                 raise Exception(e)
@@ -426,32 +317,6 @@ class TelegramStorage:
 
     def commit(self):
         self.db.commit()
-
-    def get_playlist(self, chat_id):
-        playlist = self.db.execute(
-            "SELECT * FROM `playlist` WHERE chat_id = '%s' LIMIT 1" % chat_id)
-        result = playlist.fetchone()
-        if result:
-            _, chat_id, title, original_title, first_message_id, last_message_id = result
-            return {"chat_id": chat_id, "title": title, "original_title": original_title,
-                "first_message_id": first_message_id, "last_message_id": last_message_id}
-        return None
-
-    def save_playlist(self, chat_id, data):
-        playlist = self.get_playlist(chat_id)
-        is_update = playlist is not None
-        playlist = {
-            **(playlist if playlist else {}),
-            **data
-        }
-        if is_update:
-            return self.update('playlist', playlist, {"chat_id": chat_id}, 1)
-        return self.insert('playlist', playlist)
-
-    # def get_any_audio(self):
-    #     audio = self.db.execute('SELECT * FROM `audio` WHERE 1=1')
-    #     return audio.fetchall()
-    #     # return audio
 
     def select(self, table, where, limit=1):
         set_where = []
@@ -474,7 +339,6 @@ class TelegramStorage:
         for k in where.keys():
             set_where.append(f'{k} = ?')
             set_values.append(where[k])
-#         set_keys = ', '.join(set_keys)
         set_where = ' AND '.join(set_where)
         sql = f"UPDATE `{table}` SET {set_keys} WHERE {set_where}"
         if limit and limit > 0:
@@ -513,26 +377,26 @@ class TelegramStorage:
         set_keys = ', '.join(set_keys)
         return set_keys, set_values
 
-    def update_audio(self, chat_id, message_id, data):
-        # @TODO remove ?
-        if not data:
-            return
-        set_keys = []
-        set_values = []
-        for k in data.keys():
-            set_keys.append(f'`{k}` = ?')
-            set_values.append(data[k])
-        set_keys = ', '.join(set_keys)
-        set_values.append(chat_id)
-        set_values.append(message_id)
-
-        self.db_cur.execute(
-            f"UPDATE `audio` SET {set_keys} WHERE chat_id = ? and message_id = ? LIMIT 1",
-            tuple(set_values)
-        )
-        result = self.db_cur.rowcount > 0
-        self.db.commit()
-        return result
+#     def update_audio(self, chat_id, message_id, data):
+#         # @TODO remove ?
+#         if not data:
+#             return
+#         set_keys = []
+#         set_values = []
+#         for k in data.keys():
+#             set_keys.append(f'`{k}` = ?')
+#             set_values.append(data[k])
+#         set_keys = ', '.join(set_keys)
+#         set_values.append(chat_id)
+#         set_values.append(message_id)
+#
+#         self.db_cur.execute(
+#             f"UPDATE `audio` SET {set_keys} WHERE chat_id = ? and message_id = ? LIMIT 1",
+#             tuple(set_values)
+#         )
+#         result = self.db_cur.rowcount > 0
+#         self.db.commit()
+#         return result
 
     def get_audio(self, chat_id, message_id, convert=True):
         audio = self.db.execute(
@@ -554,13 +418,10 @@ class TelegramStorage:
         return result
 
     def add_audio(self, data, convert=True, commit=True):
-#         print('============add_audio===========')
-#         print(data)
         if not ('audio' in data['content'] and audio_content_set <= set(data['content']['audio'])):
             logger.warning('Audio message has no required keys, skipping...')
             print(data['content'])
             return
-
         d = {}
         content = data['content']
         audio = content['audio']
@@ -584,7 +445,6 @@ class TelegramStorage:
         d['local_path'] = local['path']
         d['is_downloaded'] = 1 if local['is_downloading_completed'] else 0
 
-#         print('=========EXECUTE INSERT AUDIO========')
         self.db_cur.execute("""
             INSERT INTO `audio` (
                 chat_id, message_id, mime_type, title, artist, file_name, date, size, duration,
@@ -594,41 +454,37 @@ class TelegramStorage:
                 :local_path, :is_downloaded)
         """, d)
 
-        # logger.info('Audio added %d', self.db_cur.lastrowid)
-#         print('Audio added %s', self.db_cur.lastrowid)
-#         print('Audio added %s', d['message_id'])
         d['id'] = self.db_cur.lastrowid
-#         print(d)
         result = d if not convert else TgAudio(d)
         if commit:
             self.db.commit()
         return result
 
-    def add_photo(self, data):
-        if not ('photo' in data['content'] and 'sizes' in data['content']['photo']):
-            logger.warning('Photo message content has no required keys, skipping...')
-            print(data['content'])
-            return
-
-        d = {}
-        content = data['content']
-        photo = content['photo']['sizes'][0]
-
-        d['info'] = ''
-        d['caption'] = ''
-        d['chat_id'] = data['chat_id']
-        d['message_id'] = data['id']
-        d['date'] = data['date']
-
-        if 'caption' in content and 'text' in content['caption']:
-            caption = content['caption']['text']
-            info = parse_info(caption)
-            d['info'] = json.dumps(info)
-
-        self.db_cur.execute("""
-            INSERT INTO `info` (chat_id, message_id, caption, date, info)
-            VALUES (:chat_id, :message_id, :caption, :date, :info)
-        """, d)
-
-        logger.info('Info added %d', self.db_cur.lastrowid)
-        return self.db_cur.lastrowid
+#     def add_photo(self, data):
+#         if not ('photo' in data['content'] and 'sizes' in data['content']['photo']):
+#             logger.warning('Photo message content has no required keys, skipping...')
+#             print(data['content'])
+#             return
+#
+#         d = {}
+#         content = data['content']
+#         photo = content['photo']['sizes'][0]
+#
+#         d['info'] = ''
+#         d['caption'] = ''
+#         d['chat_id'] = data['chat_id']
+#         d['message_id'] = data['id']
+#         d['date'] = data['date']
+#
+#         if 'caption' in content and 'text' in content['caption']:
+#             caption = content['caption']['text']
+#             info = parse_info(caption)
+#             d['info'] = json.dumps(info)
+#
+#         self.db_cur.execute("""
+#             INSERT INTO `info` (chat_id, message_id, caption, date, info)
+#             VALUES (:chat_id, :message_id, :caption, :date, :info)
+#         """, d)
+#
+#         logger.info('Info added %d', self.db_cur.lastrowid)
+#         return self.db_cur.lastrowid
