73547d8ff5f8b6e9ddb1f6b03c66853db2d547d5
Andrey Izman <izmanw@gmail.com>
2024-06-08 18:00:16 +0300

    Improve downloading audio

--- TelegramEntry.py
+++ TelegramEntry.py
@@ -14,11 +14,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import RB, GLib
+from gi.repository import RB, GLib, GObject
 from common import file_uri, get_location_data, TG_RhythmDBPropType
 
 
 class TelegramEntryType(RB.RhythmDBEntryType):
+    __gsignals__ = {
+        'entry_downloaded': (GObject.SIGNAL_RUN_FIRST, None, (GObject.TYPE_PYOBJECT,)),
+    }
+
     def __init__(self, plugin):
         RB.RhythmDBEntryType.__init__(self, name='telegram')
         self.source = None
@@ -26,15 +30,43 @@ class TelegramEntryType(RB.RhythmDBEntryType):
         self.shell = plugin.shell
         self.db = plugin.db
         self.shell_player = self.shell.props.shell_player
-        self._set_entry = None
+        self._pending_playback_entry = None
         self._was_stopped = None
         self._entry_error_id = None
+        self._entry_downloaded_id = None
 
     def activate(self):
         self._entry_error_id = self.shell_player.props.player.connect('error', self._on_player_error)
+        self._entry_downloaded_id = self.connect('entry_downloaded', self._on_entry_downloaded)
 
     def deactivate(self):
         self.shell_player.props.player.disconnect(self._entry_error_id)
+        self.disconnect(self._entry_downloaded_id)
+
+    def _on_entry_downloaded(self, entry_type, entry):
+        print('entry')
+        print(entry)
+        # print(*args)
+        playing_entry = self._pending_playback_entry
+        print('playing_entry')
+        print(playing_entry)
+
+        if not playing_entry:
+            return
+
+        print('playing_entry')
+        print(playing_entry)
+        playing_location = playing_entry.get_string(RB.RhythmDBPropType.LOCATION) if playing_entry else None
+        print(playing_location)
+
+        location = entry.get_string(RB.RhythmDBPropType.LOCATION)
+        if location == playing_location:
+            self._pending_playback_entry = None
+            self.shell_player.play_entry(entry, self.plugin.source)
+
+            # if self._was_stopped:
+            #     self._was_stopped = False
+            GLib.timeout_add(100, self.shell.props.shell_player.emit, "playing-changed", True)
 
     def _on_player_error(self, *args):
         self._was_stopped = True
@@ -62,6 +94,9 @@ class TelegramEntryType(RB.RhythmDBEntryType):
             iter = entry_view.iter_next(iter)
         return None
 
+    def _download_entry(self, entry):
+        self.plugin.loader.add_entry(entry).start()
+
     def _get_playback_uri(self, entry):
         location = entry.get_string(RB.RhythmDBPropType.LOCATION)
         chat_id, message_id = get_location_data(location)
@@ -73,14 +108,11 @@ class TelegramEntryType(RB.RhythmDBEntryType):
         if next_entry:
             next_audio = self.plugin.storage.get_entry_audio(next_entry)
             if next_audio and not next_audio.is_file_exists():
-                self.plugin.loader.add_entry(next_entry).start()
+                GLib.idle_add(self._download_entry, next_entry)
 
         if audio.is_file_exists():
-            self._set_entry = None
+            self._pending_playback_entry = None
             return file_uri(audio.local_path)
-
-        # if self.plugin.loader.is_downloading(entry):
-        #     return None
 
         return_val = None
 
@@ -92,40 +124,12 @@ class TelegramEntryType(RB.RhythmDBEntryType):
             GLib.idle_add(self.shell.props.shell_player.stop)
             return_val = 'invalid'
 
-        if self.plugin.is_downloading:
-            return return_val
-
         state = entry.get_string(TG_RhythmDBPropType.STATE)
         if state == 'STATE_LOADING':
             return return_val
 
-        self.plugin.is_downloading = True
-        self._set_entry = entry
-        self.db.entry_set(entry, TG_RhythmDBPropType.STATE, 'STATE_LOADING')
-        self.db.commit()
-
-        def success(upd_audio):
-            self.plugin.is_downloading = False
-            upd_audio.update_tags()
-            upd_audio.update_entry(entry)
-            if self._set_entry is not None:
-                play_entry = self._set_entry
-                self._set_entry = None
-                self.shell_player.play_entry(play_entry, self.plugin.source)
-
-                if self._was_stopped:
-                    self._was_stopped = False
-                    GLib.timeout_add(100, self.shell.props.shell_player.emit, "playing-changed", True)
-
-        def fail():
-            audio.is_error = True
-            self.db.entry_set(entry, RB.RhythmDBPropType.PLAYBACK_ERROR, 1)
-            self.db.entry_set(entry, TG_RhythmDBPropType.STATE, 'STATE_ERROR')
-            self.db.commit()
-            self.plugin.is_downloading = False
-            self._set_entry = None
-
-        audio.download(success=success, fail=fail)
+        self._pending_playback_entry = entry
+        self._download_entry(entry)
         return return_val
 
     def do_can_sync_metadata(self, entry): # noqa
