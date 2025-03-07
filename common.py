b753d8dde62413ba6638edbbf36edd580aee45c8
Andrey Izman <izmanw@gmail.com>
2025-03-07 20:52:48 +0200

    Remove unused functions, add get_tree_view_from_entry_view()

--- common.py
+++ common.py
@@ -26,6 +26,10 @@ gettext.install('rhythmbox', RB.locale_dir())
 
 
 class SingletonMeta(type):
+    """
+    A metaclass that ensures a class follows the Singleton design pattern.
+    This means that only one instance of the class will be created and reused.
+    """
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
@@ -35,6 +39,7 @@ class SingletonMeta(type):
 
 
 class MessageType(enum.Enum):
+    """ Enum representing the types of messages that can be handled. """
     NONE = None
     PHOTO = 'messagePhoto'
     DOCUMENT = 'messageDocument'
@@ -42,6 +47,7 @@ class MessageType(enum.Enum):
 
     @staticmethod
     def has(value):
+        """ Checks if a given value is a valid MessageType. """
         return value in message_type_set
 
 
@@ -56,6 +62,7 @@ message_set = {'id', 'chat_id', 'date', 'content'}
 audio_content_set = {'mime_type', 'file_name', 'performer', 'title', 'duration', 'audio'}
 photo_content_set = {'sizes'}
 
+# Support audio mime types dict
 mime_types = {
     # supported by Telegram (type: messageAudio)
     'audio/m4a': 'm4a',
@@ -99,6 +106,7 @@ mime_types = {
     'audio/x-aiff': 'aiff',
 }
 
+# Telegram API errors
 API_ERRORS = {
     'FIRSTNAME_INVALID': _('The first name is invalid'),
     'LASTNAME_INVALID': _('The last name is invalid'),
@@ -129,61 +137,70 @@ API_ERRORS = {
 }
 
 def get_entry_state(entry):
+    """ Retrieves the state of a given entry. """
     return entry.get_ulong(RB.RhythmDBPropType.MTIME)
 
 def set_entry_state(db, entry, state):
+    """ Sets the state of a given entry. """
     db.entry_set(entry, RB.RhythmDBPropType.MTIME, state)
 
 def get_entry_location(entry):
+    """ Retrieves the file URI of a given entry. """
     return entry.get_string(RB.RhythmDBPropType.LOCATION)
 
-def is_same_entry(entry1, entry2):
-    return get_entry_location(entry1) == get_entry_location(entry2)
-
 def to_location(api_hash, chat_id, message_id, audio_id):
+    """ Constructs a location string for a Telegram audio file. """
     return 'tg://%s/%s/%s/%s' % (api_hash, chat_id, message_id, audio_id)
 
 def get_location_audio_id(location):
+    """ Extracts the audio ID from a location string. """
     return location.split('/')[-1]
 
 def get_location_data(location):
+    """ Extracts the chat ID and message ID from a location string. """
     d = location.split('/')
     return [d[-3], d[-2]]
 
 def file_uri(path):
+    """ Converts a file path to a URI. """
     return GLib.filename_to_uri(path, None)
 
 def open_path(path):
+    """ Opens a file path using the default application. """
     Gio.app_info_get_default_for_uri(file_uri(path), None)
 
 def get_content_type(data):
+    """ Determines the content-type of a message. """
     if '@type' in data['content'] and MessageType.has(data['content']['@type']):
         return MessageType(data['content']['@type'])
     return MessageType.NONE
 
 def is_msg_valid(data):
+    """ Checks if a message contains all required fields. """
     return message_set <= set(data)
 
 def get_chat_info(chat):
+    """ Extracts the ID and title from a chat object. """
     return {
         'id': chat['id'],
         'title': chat['title'],
     }
 
-def timestamp():
-    return datetime.timestamp(datetime.now())
-
 def empty_cb(*args): # noqa
+    """ A no-op callback function that does nothing. """
     pass
 
 def cb(fn):
+    """ Returns the given function or returns a no-op callback. """
     return fn if fn else empty_cb
 
 def get_option_title(options, value):
+    """ Retrieves the title of an option based on its value. """
     for option in options:
         if option[1] == value:
             return option[0]
 
+# Audio meta tags dict
 META_TAGS = {
     'title': RB.MetaDataField.TITLE,
     'artist': RB.MetaDataField.ARTIST,
@@ -196,6 +213,7 @@ META_TAGS = {
 }
 
 def get_audio_tags(file_path):
+    """ Retrieves metadata tags from an audio file. """
     tags = {}
     metadata = RB.MetaData()
     uri = GLib.filename_to_uri(file_path, None)
@@ -226,6 +244,7 @@ filename_illegal1 = '<>/\\|*'
 filename_illegal2 = '":'
 
 def clear_filename(filename):
+    """ Cleans a filename by removing illegal characters. """
     for char in filename_illegal1:
         filename = filename.replace(char, '_')
     for char in filename_illegal2:
@@ -265,6 +284,7 @@ filepath_pattern_markers = {
 }
 
 def get_first_artist(artist, extra_separators=None):
+    """ Extracts the first artist from a string that may contain multiple artists. """
     artist = artist.split('\x01')[0].split(';')[0]
     if extra_separators:
         for separator in list(extra_separators):
@@ -274,12 +294,14 @@ def get_first_artist(artist, extra_separators=None):
 RE_FEAT = re.compile(r'\(feat\.|\(feat |\(featuring |\[feat\.')
 
 def get_base_title(title):
+    """ Extracts the base title from a string that may contain additional information. """
     match = RE_FEAT.search(title)
     return title[:match.start()].strip() if match else title
 
 RE_TRACK_NUM = re.compile(r'^\d{1,3}')
 
 def extract_track_number(filename):
+    """ Extracts the track number from a filename. """
     match = RE_TRACK_NUM.match(filename)
     if match:
         track_number = int(match.group())
@@ -287,7 +309,7 @@ def extract_track_number(filename):
     return 1
 
 def filepath_parse_pattern(pattern, tags):
-    # Parse a filename pattern and replace markers with values from the tags
+    """ Parses a filename pattern and replaces markers with values from the tags. """
     _tags = {**tags}
     # Remove the '\x01\x02' characters from the string, for some reason some artist have 2 or
     # maybe even more lines separated by \x01\x02
@@ -305,15 +327,18 @@ def filepath_parse_pattern(pattern, tags):
     return pattern
 
 def get_date(unix_ts):
+    """ Converts a Unix timestamp to a Julian date. """
     dt = GLib.DateTime.new_from_unix_local(int(unix_ts))
     date = GLib.Date.new_dmy(dt.get_day_of_month(), GLib.DateMonth(dt.get_month()), dt.get_year())
     return date.get_julian()
 
 def get_year(julian):
+    """ Extracts the year from a Julian date. """
     date = GLib.Date.new_julian(julian)
     return date.get_year()
 
-def show_error(title, description=None, parent=None): # noqa
+def show_error(title, description=None, parent=None):
+    """ Displays an error dialog with the given title and description. """
     if parent is not None:
         if not isinstance(parent, Gtk.Window):
             parent = parent.get_toplevel()
@@ -332,6 +357,7 @@ def show_error(title, description=None, parent=None): # noqa
     err_dialog.destroy()
 
 def pretty_file_size(size_bytes, digits=2):
+    """ Converts a file size in bytes to a human-readable format. """
     if size_bytes == 0:
         return "0 bytes"
     size_name = ("bytes", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
@@ -341,16 +367,19 @@ def pretty_file_size(size_bytes, digits=2):
     return f"{s} {size_name[i]}"
 
 def get_file_size(filename):
+    """ Retrieves the size of a file. """
     file = Gio.File.new_for_path(filename)
     info = file.query_info('standard::size', Gio.FileQueryInfoFlags.NONE)
     return int(info.get_size())
 
 def format_time(seconds):
+    """ Formats a duration in seconds to a human-readable time format. """
     if seconds < 3600:
         return f"{seconds // 60:01}:{seconds % 60:02}"
     return f"{seconds // 3600:01}:{(seconds % 3600) // 60:02}:{seconds % 60:02}"
 
 def get_window_center(window):
+    """ Calculates the center coordinates of a window. """
     if isinstance(window, Gtk.ApplicationWindow):
         width, height = window.get_size()
         x, y = window.get_position()
@@ -364,6 +393,7 @@ def get_window_center(window):
     return top_center, left_center
 
 def move_window_center(window, parent):
+    """ Moves a window to the center of its parent window. """
     x11window = window.get_toplevel().get_property('window')
     geometry = x11window.get_geometry()
     top_center, left_center = get_window_center(parent)
@@ -372,7 +402,21 @@ def move_window_center(window, parent):
     window.move(left, top)
 
 def version_to_number(version: str) -> int:
+    """ Converts a version string to a numerical representation. """
     parts = [int(x) for x in version.split(".")]
     while len(parts) < 3:
         parts.append(0)
     return parts[0] * 10**6 + parts[1] * 10**3 + parts[2]
+
+def get_tree_view_from_entry_view(entry_view):
+    """ Finds and returns the Gtk.TreeView widget within an entry view. """
+    def find_tree_view(widget):
+        if isinstance(widget, Gtk.TreeView):
+            return widget
+        if hasattr(widget, 'get_children'):
+            for child in widget.get_children():
+                result = find_tree_view(child)
+                if result:
+                    return result
+        return None
+    return find_tree_view(entry_view)
